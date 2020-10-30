b88069eacff24b8bf00c30f7afaa4e47481ee77c
Nicom7910 <63812959+Nicom7910@users.noreply.github.com>
2020-10-29 21:27:40 -0300

    Create validacion.js

---
+++ validacion.js
@@ -0,0 +1,36 @@
+function validar() {
+
+    var nombre,mail,contra;
+
+    nombre=document.getElementById("nombre").value;
+    mail=document.getElementById("correo").value;
+    contra=document.getElementById("contra").value;
+
+    if (nombre === null || mail === null|| contra === null){
+        alert("El campo nombre se encuentra vacio");
+        return false;
+    }
+
+    if( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(mail)) ) {
+	return false;
+	}
+	
+	else{
+		alert("Hola");
+	}
+}
+
+}
+function validar1() {
+
+    var mail,contra;
+
+    mail=document.getElementById("email").value;
+    contra=document.getElementById("contraseña").value;
+    
+    if (mail === null|| contra === null){
+        alert("El campo nombre se encuentra vacio");
+        return false;
+    }
+
+}
